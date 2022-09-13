fe967101559186ba0730895e2013e8ec7d8d6213
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-13 12:37:41 -0400

    update component routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 
+import {
+  Actors,
+  MovieInformation,
+  Movies,
+  Navbar,
+  Profile,
+} from "./components";
+
 const App = () => {
   return (
     <div>
       <CssBaseline />
+      {/* {<Navbar />} */}
       <main>
         <Routes>
-          <Route path="/" element={<h1>Movies</h1>} />
-          <Route path="/movie/:id" element={<h1>Movie Information</h1>} />
-          <Route path="/actors/:id" element={<h1>Actors</h1>} />
-          <Route path="/profile/:id" element={<h1>Profile</h1>} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
         </Routes>
       </main>
     </div>
--- src/components/Profiles/Profiles.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Profiles = () => {
-  console.log("Profiles");
-
-  return <div>Profiles</div>;
-};
-
-export default Profiles;
